a7a8aa1ba35aa9f17a39d51df23e2412a428c65a
Jan Svoboda <jan@mluv.cz>
2015-09-23 17:59:34 +0200

    add permissions

--- init.rb
+++ init.rb
@@ -6,10 +6,22 @@ Redmine::Plugin.register :wiki_html_macro do
   url 'http://www.chiliproject.eu/projects/chiliproject-eu/wiki/Wiki_HTML_Macro_Plugin'
   author_url 'http://www.chiliproject.eu'
 
+  permission :use_html_macro, {}
+
   Redmine::WikiFormatting::Macros.register do
     desc "Insert html\n\n  !{{html\n<HTML CODE HERE>\n}}"
     macro :html do |obj, args, text|
-      text.html_safe
+      if obj.respond_to?(:author) && obj.author.present? &&
+        (obj.author.admin? || obj.author.allowed_to?(:use_html_macro, obj.try(:project)))
+
+        text.html_safe
+      elsif obj.respond_to?(:user) && obj.user.present? &&
+        (obj.user.admin? || obj.user.allowed_to?(:use_html_macro, obj.try(:project)))
+
+        text.html_safe
+      else
+        "ERROR: HTML content macro not allowed: #{text}"
+      end
     end
   end
 end
